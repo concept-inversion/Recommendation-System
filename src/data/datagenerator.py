4d9b2005ad8951a098e01f5438e9fcd50116bc9a
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-06 23:58:09 +0545

    csv module loaded

--- src/data/datagenerator.py
+++ src/data/datagenerator.py
@@ -8,7 +8,7 @@ class generateFake():
         self.interest = ['python','JS','QA','PHP']
         self.qualification = ['Bachelors','+2','Masters']
         csvfile = open('src/data/data.csv', 'wb')
-
+        csvwriter = csv.writer(csvfile,delimiter = ',')
         for i in range(10):
             row = []
             row.append(self.id)
@@ -16,8 +16,10 @@ class generateFake():
             row.append(random.choice(self.interest))
             row.append(random.choice(self.qualification))
             self.id += 1
+            csvwriter.writerow(row,.join())
+        csvfile.close()
 
 if __name__ == '__main__':
     fak = generateFake()
-    Total = fak.Fake()
-    print(Total)
+    fak.Fake()
+    
