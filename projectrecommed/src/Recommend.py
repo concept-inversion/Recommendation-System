45f4a8906e8b48a0b4b62ebe63e657a37cbfd785
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-10 23:44:28 +0545

    main recommend module made

--- projectrecommed/src/Recommend.py
+++ projectrecommed/src/Recommend.py
@@ -2,23 +2,27 @@ import csv
 import pandas as pd
 import os
 from Recommenders.Collaborative import CollaborativeRec
+
 class Recommend():
     def __init__(self, *args, **kwargs):
         self.userId= args[0]
         self.csvReader()
-        
-        #self.collab= CollaborativeRec()
-        
-    
+        self.collab= CollaborativeRec(self.userdata,self.jobdata, self.test,self.userId)
+
     def getData(self,id):
         #Read test,user,job json data or csv data
         #call collab
         #return json
         pass
+
     def csvReader(self):
-        userdata = pd.read_csv('projectrecommed/src/data/finalUser.csv')
-        jobdata = pd.read_csv('projectrecommed/src/data/finalJob.csv')
-        
+        self.userdata = pd.read_csv('projectrecommed/src/data/finalUser.csv')
+        self.userdata.set_index('UserId',inplace = True)
+        self.jobdata = pd.read_csv('projectrecommed/src/data/finalJob.csv')
+        self.jobdata.set_index('Jobid',inplace = True)
+        self.test= self.userdata.iloc[[1]]
+        print(self.userdata.head())
+
 if __name__ == '__main__':
     print(os.getcwd())
-    a = Recommend('hy')
+    a = Recommend(23)
