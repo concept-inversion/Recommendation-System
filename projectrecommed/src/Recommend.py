441828c3b16bb7185020aea1d726658d8d256660
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-10 18:03:15 +0545

    files added

--- projectrecommed/src/Recommend.py
+++ projectrecommed/src/Recommend.py
@@ -1,1 +1,22 @@
-from surprise import SVD
+import csv
+import pandas as pd
+#from src.Recommenders.Collaborative import CollaborativeRec
+from .Recommenders.Collaborative import CollaborativeRec
+class Recommend():
+    def __init__(self, *args, **kwargs):
+        self.userId= args[0]
+        self.csvReader()
+        #self.collab= CollaborativeRec()
+        
+    
+    def getData(self,id):
+        #Read test,user,job json data or csv data
+        #call collab
+        #return json
+        pass
+    def csvReader(self):
+        userdata = pd.read_csv('/data/finalUser.csv')
+        jobdata = pd.read_csv('/data/finalJob.csv')
+
+if __name__ == '__main__':
+    a = Recommend()
