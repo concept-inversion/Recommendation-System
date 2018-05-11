c995ee3511335601bda42f85cca0663863594db9
Bindeep Acharya <bindeep.acharya@aayulogic.com>
2018-05-11 15:00:54 +0545

    data retrieval and dataframe transformation

--- projectrecommed/projectrecommed/views.py
+++ projectrecommed/projectrecommed/views.py
@@ -3,31 +3,33 @@ from django.views.generic.base import TemplateView
 import pandas as pd
 # from src.Recommend import Recommend
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 class RecommendView(TemplateView):
     template_name = 'detail.html'
 
-    # def get_context_data(self, *args, **kwargs):
-    #     req_list = ['Experience', 'php', 'python', 'qa', 'js', 'level', 'qualification', 'age', 'Jobid']
-    #     user_data_dict = self.request.GET
-    #     import ipdb; ipdb.set_trace()
-    #     user_data = {req_item : user_data_dict.get(req_item)[0] for req_item in req_list }
-
     def get(self, *args, **kwargs):
-        # req_list = ['Experience', 'php', 'python', 'qa', 'js', 'level', 'qualification', 'age',]
         user_data_order_dict = self.request.GET
+
+        # change ordered dict to dict
         user_dict = {k :int(v[0]) for k,v in dict(user_data_order_dict).items()}
-        print(user_dict)
-        pd_dataframe = pd.DataFrame.from_dict(user_dict.update({'id': 1}))
-        print(pd_dataframe.set_index('id', inplace=True))
+
+        # change user dictionay to pandas dataframe
+        pd_dataframe = pd.DataFrame.from_dict([user_dict])
+
+        # retrive all data context data
+        context = super().get_context_data()
+
+        # context['similar_jobs']
+        return self.render_to_response(context)
+    
 
 
 
-# dictr = {'php': '1', 'python': '1', 'qualification': '2', 'experience': '5', 'level': '2', 'age': '25'}
-
-# frame = pd.DataFrame.from_dict(dictr)
-# print(frame)
+columns = ['Experience', 'php', 'python', 'qa', 'js', 'level', 'qualification', 'age',]
 dictr = {'php': 1, 'python': 1, 'qualification': 2, 'experience': 5,'level': 2, 'age': 25}
-frame = pd.DataFrame.from_dict(dictr)
+frame = pd.DataFrame.from_dict([dictr])
+print(frame)
