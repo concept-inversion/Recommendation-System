c26ee9782662e3bb29a3d4d27b1393c067b3f796
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-11 17:31:19 +0545

    get data and read data method

--- projectrecommed/src/Recommend.py
+++ projectrecommed/src/Recommend.py
@@ -1,27 +1,32 @@
 import csv
 import pandas as pd
 import os
-from Recommenders.Collaborative import CollaborativeRec
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from src.Recommenders.Collaborative import CollaborativeRec
+#from Recommenders.Collaborative import CollaborativeRec
 
 class Recommend():
     def __init__(self, *args, **kwargs):
         self.test= args[0]
         self.csvReader()
-        self.collab= CollaborativeRec(self.userdata,self.jobdata, self.test,self.userId)
-
-    def getData(self,id):
+        self.collab= CollaborativeRec(self.userdata,self.jobdata, self.test)
+        
+    def readData(self,id):
         #Read test,user,job json data or csv data
         #call collab
         #return json
         pass
 
+    def getData(self):
+        data = self.collab.getRecord()
+        return data
+
+
     def csvReader(self):
         #Define relative path for csv files
-        self.userdata = pd.read_csv('data/finalUser.csv')
+        self.userdata = pd.read_csv('src/data/finalUser.csv')
         self.userdata.set_index('UserId',inplace = True)
-        self.jobdata = pd.read_csv('data/finalJob.csv')
+        self.jobdata = pd.read_csv('src/data/finalJob.csv')
         self.jobdata.set_index('Jobid',inplace = True)
 
-if __name__ == '__main__':
-    print(os.getcwd())
-    a = Recommend(test)
