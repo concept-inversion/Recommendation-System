dce1295a12eb5f27f751c3c6cf95632c8b87892d
Bindeep Acharya <bindeep.acharya@aayulogic.com>
2018-05-11 13:10:20 +0545

    modified recommendation views for recommendation system

--- projectrecommed/projectrecommed/views.py
+++ projectrecommed/projectrecommed/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 import pandas as pd
+# from src.Recommend import Recommend
 
 def home(request):
     return render(request, 'home.html')
@@ -17,5 +18,16 @@ class RecommendView(TemplateView):
     def get(self, *args, **kwargs):
         # req_list = ['Experience', 'php', 'python', 'qa', 'js', 'level', 'qualification', 'age',]
         user_data_order_dict = self.request.GET
-        user_dict = {k :v[0] for k,v in dict(user_data_order_dict).items()}
-        pd_dataframe = pd.DataFrame.from_dict(user_dict)
+        user_dict = {k :int(v[0]) for k,v in dict(user_data_order_dict).items()}
+        print(user_dict)
+        pd_dataframe = pd.DataFrame.from_dict(user_dict.update({'id': 1}))
+        print(pd_dataframe.set_index('id', inplace=True))
+
+
+
+# dictr = {'php': '1', 'python': '1', 'qualification': '2', 'experience': '5', 'level': '2', 'age': '25'}
+
+# frame = pd.DataFrame.from_dict(dictr)
+# print(frame)
+dictr = {'php': 1, 'python': 1, 'qualification': 2, 'experience': 5,'level': 2, 'age': 25}
+frame = pd.DataFrame.from_dict(dictr)
