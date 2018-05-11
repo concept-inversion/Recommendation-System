511a99f371a9297e020952656d3e52b23b6a48f6
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-11 17:32:27 +0545

    connected with recommend sys

--- projectrecommed/projectrecommed/views.py
+++ projectrecommed/projectrecommed/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 import pandas as pd
-# from src.Recommend import Recommend
+from src.Recommend import Recommend
 
 
 def home(request):
@@ -19,7 +19,11 @@ class RecommendView(TemplateView):
 
         # change user dictionay to pandas dataframe
         pd_dataframe = pd.DataFrame.from_dict([user_dict])
-
+        
+        # get job from Recommend
+        similar = Recommend(pd_dataframe)
+        data= similar.getData()
+        
         # retrive all data context data
         context = super().get_context_data()
 
@@ -32,4 +36,3 @@ class RecommendView(TemplateView):
 columns = ['Experience', 'php', 'python', 'qa', 'js', 'level', 'qualification', 'age',]
 dictr = {'php': 1, 'python': 1, 'qualification': 2, 'experience': 5,'level': 2, 'age': 25}
 frame = pd.DataFrame.from_dict([dictr])
-print(frame)
