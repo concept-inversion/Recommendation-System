3b35b5488c9a7042a708a24affa8ce1413278455
Bindeep Acharya <bindeep.acharya@aayulogic.com>
2018-05-13 21:22:48 +0545

    json parser for user is created

---
+++ projectrecommed/src/data/UserJsonParser.py
@@ -0,0 +1,317 @@
+data = """{  
+    "id":6,
+    "gender":"Male",
+    "job_level":"Top Level",
+    "available_for":[  
+       "Full Time"
+    ],
+    "current_address":{  
+       "address":"Kathmandu, Nepal",
+       "street":"Kathmandu, Nepal",
+       "city":"Kathmandu",
+       "country":"Nepal",
+       "latitude":27.7172453,
+       "longitude":85.3239605
+    },
+    "user":{  
+       "id":10,
+       "email":"shrawan.poudel@merojob.com",
+       "username":"shrawan.poudel08",
+       "name":"Shrawan Poudel",
+       "form_completed":true,
+       "display_picture":"http://192.168.99.200/media/uploads/profile-pics/00040e5d-1eb8-4dbf-98a6-e7e86f932b0c.jpg",
+       "user_category":"Jobseeker",
+       "date_joined":"2018-04-23T08:43:08.142359Z",
+       "status":"Active",
+       "last_login":"2018-05-08T09:35:15.055836Z"
+    },
+    "dob":"1995-07-12",
+    "experience_months":193,
+    "created_at":"2018-04-23T08:43:08.234389Z",
+    "skills":[  
+       "Python"
+    ],
+    "job_preference_locations":[  
+       {  
+          "address":"Kathmandu, Nepal",
+          "street":"Kathmandu, Nepal",
+          "city":"Kathmandu",
+          "country":"Nepal",
+          "latitude":27.7172453,
+          "longitude":85.3239605
+       }
+    ],
+    "nationality":"Nepali",
+    "marital_status":"Unmarried",
+    "is_disabled":false,
+    "disabilities":[  
+ 
+    ],
+    "religion":"Hinduism",
+    "website":null,
+    "bio":"Hello this is career objective",
+    "specializations":[  
+       "Python"
+    ],
+    "special_qualifications":"None",
+    "current_salary":null,
+    "industries":[  
+       "Information / Computer / Technology"
+    ],
+    "job_categories":[  
+       "IT and Telecommunication"
+    ],
+    "expected_salary":{  
+       "currency":"NRs",
+       "operator":"Above",
+       "minimum":10.0,
+       "unit":"Monthly",
+       "maximum":null
+    },
+    "view_count":null,
+    "rating":0.0,
+    "permanent_address":{  
+       "address":"Kathmandu, Nepal",
+       "street":"Kathmandu, Nepal",
+       "city":"Kathmandu",
+       "country":"Nepal",
+       "latitude":27.7172453,
+       "longitude":85.3239605
+    },
+    "references":[  
+ 
+    ],
+    "profile_views":[  
+       {  
+          "id":2,
+          "name":"Aayulogic",
+          "display_picture":"/media/default_pp/employer/employer.png",
+          "created_at":"2018-04-23T08:57:05.815931Z",
+          "user_category":"Employer"
+       }
+    ],
+    "social_accounts":[  
+ 
+    ],
+    "user_educations":[  
+       {  
+          "id":2,
+          "institution":{  
+             "id":2,
+             "name":"Tu"
+          },
+          "program":{  
+             "id":1,
+             "name":"Master in Business Studies (MBS)",
+             "degree":{  
+                "id":1,
+                "name":"Masters"
+             }
+          },
+          "board":"Tu",
+          "grade_type":"Percentage",
+          "marks_secured":"90",
+          "is_current":false,
+          "year":2002,
+          "month":3
+       }
+    ],
+    "user_trainings":[  
+       {  
+          "id":1,
+          "training":"Python",
+          "institution":{  
+             "id":3,
+             "name":"Python",
+             "institution_type":"Vocation"
+          },
+          "duration":30,
+          "duration_type":"Week",
+          "objective":"",
+          "completion_date":{  
+             "year":2003,
+             "month":3,
+             "day":null
+          }
+       }
+    ],
+    "work_experiences":[  
+       {  
+          "id":1,
+          "org_name":"Merojob",
+          "hide_org_name":false,
+          "industry":"Airlines / GSA",
+          "designation":"Senior Programmer",
+          "job_level":"Top Level",
+          "available_for":"Full Time",
+          "job_category":"Accounting / Finance",
+          "job_location":{  
+             "address":"Kathmandu, Nepal",
+             "street":"Kathmandu, Nepal",
+             "city":"Kathmandu",
+             "country":"Nepal",
+             "latitude":27.7172453,
+             "longitude":85.3239605
+          },
+          "job_description":"sad",
+          "is_current":false,
+          "start_year":2002,
+          "start_month":5,
+          "start_day":null,
+          "end_year":2018,
+          "end_month":3,
+          "end_day":null
+       }
+    ],
+    "userotherinfo":null,
+    "usersetting":{  
+       "id":1543,
+       "profile_searchable":false,
+       "want_job_alert":true,
+       "profile_confidential":true
+    },
+    "resume_downloads":[  
+       {  
+          "id":6,
+          "name":"Aayulogic",
+          "display_picture":"/media/default_pp/employer/employer.png",
+          "created_at":"2018-04-23T10:01:03.900251Z",
+          "user_category":"Employer"
+       },
+       {  
+          "id":5,
+          "name":"Aayulogic",
+          "display_picture":"/media/default_pp/employer/employer.png",
+          "created_at":"2018-04-23T10:01:03.774845Z",
+          "user_category":"Employer"
+       },
+       {  
+          "id":2,
+          "name":"Aayulogic",
+          "display_picture":"/media/default_pp/employer/employer.png",
+          "created_at":"2018-04-23T08:56:11.634273Z",
+          "user_category":"Employer"
+       },
+       {  
+          "id":1,
+          "name":"Aayulogic",
+          "display_picture":"/media/default_pp/employer/employer.png",
+          "created_at":"2018-04-23T08:56:11.527891Z",
+          "user_category":"Employer"
+       }
+    ],
+    "user_favorite_by":[  
+ 
+    ],
+    "languages":[  
+ 
+    ],
+    "phone_numbers":[  
+       {  
+          "id":6,
+          "number":"9843066865",
+          "number_type":"Mobile"
+       }
+    ],
+    "applied_jobs":[  
+       {  
+          "id":1,
+          "job":{  
+             "id":2,
+             "title":"Senior Python Dev",
+             "slug":"senior-python-dev",
+             "client":{  
+                "client_name":"Aayulogic",
+                "id":3,
+                "slug":"aayulogic"
+             },
+             "job_level":"Senior Level",
+             "job_service":{  
+                "name":"Top Job",
+                "service_type":"Premium Job",
+                "level":1
+             },
+             "job_locations":[  
+                {  
+                   "address":"",
+                   "street":null,
+                   "city":null,
+                   "country":null,
+                   "latitude":null,
+                   "longitude":null
+                }
+             ],
+             "logo":{  
+                "url":"/media/default_pp/employer/employer.png",
+                "image":"default_pp/employer/employer.png"
+             }
+          },
+          "created_at":"2018-04-23T08:54:07.954887Z",
+          "status":"Screened"
+       }
+    ],
+    "documents":[  
+ 
+    ],
+    "display_picture":"/media/default_pp/jobseeker/male.png"
+ }"""
+
+
+import json
+from datetime import datetime
+
+json_data = json.loads(data)
+
+class JsonParser:
+
+    # def __init__(self, *args, **kwargs):
+    #     self.json_data = args[0]
+    
+    def get_age(self, dob):
+        this_year = datetime.now().year
+        date_of_bith = datetime.strptime(dob, '%Y-%m-%d').year
+        return this_year - date_of_bith
+
+    def parse(self, json_data):
+
+        # specific field we are interested in
+        required_list = ['user','user_educations', 'skills', 'specializations', 'experience_months', 'job_level', 'dob']
+
+        # data according to specific field
+        reduced_data = {str(items):json_data.get(items) for items in required_list}
+
+
+        # reduced dictionary
+        reduced_dict = {
+            'id' : reduced_data.get('user').get('id'),
+            'skills' : reduced_data.get('skills'),
+            'specializations' : reduced_data.get('specializations'),
+            'experience' : round(reduced_data.get('experience_months')/12),
+            'educations' : reduced_data.get('user_educations')[0].get('program').get('name').lower(),
+            'level' : reduced_data.get('job_level'),
+            'dob' : reduced_data.get('dob'),
+        }
+
+        # final content of user
+        education = reduced_dict.get('educations').lower()
+        skills_list = list(map(lambda x:x.lower(), reduced_dict.get('skills')))
+        specilization_list = list(map(lambda x:x.lower(), reduced_dict.get('specializations')))
+        level = reduced_dict.get('level')
+        age = self.get_age(reduced_dict.get('dob'))
+
+        # return quantized user data in dictionary form
+        return {
+        'experience': reduced_dict.get('experience'),
+        'php': (1 if 'php' in skills_list else 0) * (2 if 'php' in specilization_list else 1),
+        'python': (1 if 'python' in skills_list else 0) * (2 if 'python' in specilization_list else 1),
+        'qa': (1 if 'qa' in skills_list else 0) * (2 if 'qa' in specilization_list else 1),
+        'js': (1 if 'js' in skills_list else 0) * (2 if 'js' in specilization_list else 1),
+        'level' : 3 if level=='Top Level' else 2 if level=='Mid Level' else 1,
+        'qualification': 3 if 'master' in education else 2 if 'bachelor' in education else 1,
+        'age' : age
+        }
+        
+
+a = JsonParser()
+b = a.parse(json_data)
+print(b)
