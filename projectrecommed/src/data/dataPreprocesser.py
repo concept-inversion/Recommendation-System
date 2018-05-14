8c1d7556def4e340824ad34d5bf06a61f54ac080
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-14 12:28:58 +0545

    makeList added

--- projectrecommed/src/data/dataPreprocesser.py
+++ projectrecommed/src/data/dataPreprocesser.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 class preProcessor():
     def __init__(self, *args, **kwargs):
+        self.jobdata = args[0]
         pass
     
     def columnSelector (self,traindata,testdata):
@@ -12,6 +13,20 @@ class preProcessor():
         testdata = testdata[order]
         return traindata,testdata
     
+    def makeList(self,similar):
+        jobList=[]
+        df= pd.DataFrame(similar)
+        df['freq'] = df.groupby(0)[0].transform('count')
+        df.drop_duplicates(0)
+        df.values.tolist()
+        for each in similar:
+            job = self.jobdata.loc[each[0]]
+            jobdict= job.to_dict()
+            jobdict['Jobid']= each[0]
+            jobdict['similarity']= each[1]
+            jobList.append(jobdict)
+        return jobList
+
     def usertouser(self):
         pass
 
