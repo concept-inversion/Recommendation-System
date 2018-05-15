d323fe58c846c2d567adca0a5f4cf76cf1e72e63
Bindeep Acharya <bindeep.acharya@aayulogic.com>
2018-05-15 14:27:37 +0545

    added documentation and merge

--- projectrecommed/src/data/dataPreprocesser.py
+++ projectrecommed/src/data/dataPreprocesser.py
@@ -6,20 +6,39 @@ class preProcessor():
         pass
     
     def columnSelector (self,traindata,testdata):
+        
+        # remove same field form both datasets 
         matching = set(traindata) - set(testdata)
+
+        # change set to list
         remove= list(matching)
+
+        # remove fields in traindata i.e not in both dataset
         traindata.drop(remove,axis=1, inplace=True)
+
+        # get list of remaining columns from traindata
         order = list(traindata)
+
+        # order testdata coulms same as traindata column
         testdata = testdata[order]
+
         return traindata,testdata
     
     def makeList(self,similar):
         jobList=[]
         df= pd.DataFrame(similar)
         df['freq'] = df.groupby(0)[0].transform('count')
+<<<<<<< HEAD
         a= df.drop_duplicates(0)
         a.sort_values(1, ascending=False)
         a.values.tolist()
+=======
+        df.drop_duplicates(0)
+        ### only shows duplicate donot impact in dataframe
+        df.sort_values(1, ascending=False)
+        ## why to list not impacting aything on dataframe
+        df.values.tolist()
+>>>>>>> documentation
         for each in similar:
             job = self.jobdata.loc[each[0]]
             jobdict= job.to_dict()
