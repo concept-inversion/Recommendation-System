fa445e62058619e774b6b006e24ce9ca083fe873
Bindeep Acharya <bindeep.acharya@aayulogic.com>
2018-05-16 17:52:48 +0545

    added views for ajax implementation

--- projectrecommed/projectrecommed/views.py
+++ projectrecommed/projectrecommed/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 import pandas as pd
 from src.Recommend import Recommend
-
+from django.http import JsonResponse, HttpResponse
+import json
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.1.html')
 
 
 class RecommendView(TemplateView):
@@ -40,7 +41,46 @@ class RecommendView(TemplateView):
 
         context['similar_jobs'] = data
 
+        # import ipdb; ipdb.set_trace()
         return render(self.request, self.template_name, context)
+        # return JsonResponse(json.dumps('heheh'), safe=False)
     
     def quantify_age(self, age):
         return 1 if age > 40 else 2 if age > 35 else 3 if age > 30 else 4 if age > 25 else 5
+
+
+    
+def quantify_age(age):
+    return 1 if age > 40 else 2 if age > 35 else 3 if age > 30 else 4 if age > 25 else 5
+
+
+def reco(request):
+    if request.method == 'GET':
+        user_data_order_dict = dict(request.GET)
+
+        category = user_data_order_dict.pop('category')[0]
+
+        user_dict = {
+            k : 0 if not v[0] else quantify_age(int(v[0])) if k=='age' else int(v[0]) for k,v in dict(user_data_order_dict).items() if v[0]
+        }
+
+        # if not category == 'management':
+        #     skill_set = ['php', 'python', 'qa', 'js']
+
+        #     skill_dict = {k: user_dict.get(k, 0) for k in skill_set}
+
+        #     final_dict = {**user_dict, **skill_dict}
+
+        # change user dictionay to pandas dataframe
+        pd_dataframe = pd.DataFrame.from_dict([user_dict])
+
+        # get job from Recommend
+        similar = Recommend(pd_dataframe,category)
+        data= similar.getData()
+        
+        new_data = json.dumps(data, default=str)
+        # retrive all data context data
+        # import ipdb; ipdb.set_trace()
+        # return JsonResponse(json.dumps('heheh'), safe=False)
+        return JsonResponse(new_data, safe=False)
+        
