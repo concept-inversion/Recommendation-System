f85bba63c7c1b048763ccb9f973ff3782bbb6763
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-16 16:12:59 +0545

    scheduler added

---
+++ projectrecommed/src/scheduler.py
@@ -0,0 +1,47 @@
+import schedule
+import time
+import requests
+import json
+import csv
+from ast import literal_eval
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+endpoint = 'https://b7f7c9c9-a10f-47fe-8d88-6efdfe76df0e.mock.pstmn.io/test2'
+print(os.getcwd())
+def addtoCsv(data):
+    f = csv.writer(open("projectrecommed/src/data/test.csv", "wb+"))
+    print('loaded')
+    
+    #for data in data:
+        #Jobid,Experience,php,python,qa,js,level,qualification,company,jobtitle
+    #data['Jobid']
+    #f.writerow([
+     #   a
+        #b'data["Experience"],
+        #b'data["php"],
+        #b'data["python"],
+        #data["qa"],
+        #data["js"],
+        #data["level"],
+        #b'data["qualification"]
+        
+        
+      #  ])
+
+
+def job():
+    response =requests.get(endpoint)
+    js= response.content
+    temp= literal_eval(js.decode('utf8'))
+
+    data = json.loads(temp)
+    addtoCsv(data)
+schedule.every(1).seconds.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("12:00").do(job)
+
+while True:
+    schedule.run_pending()
+
+
+
