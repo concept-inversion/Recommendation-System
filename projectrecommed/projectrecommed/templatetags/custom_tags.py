24f8a3f2810cf52a1a575463045b0b9ba93b686e
Bindeep Acharya <bindeep.acharya@aayulogic.com>
2018-05-16 13:09:37 +0545

    added logic for dynamic rendering

--- projectrecommed/projectrecommed/templatetags/custom_tags.py
+++ projectrecommed/projectrecommed/templatetags/custom_tags.py
@@ -36,14 +36,25 @@ def order_items(data):
     # reorder values to display
     reorder = [9, 8, 7, 0, 1, 2, 3, 4, 5, 6, 10]
 
-    # get all values from list of dictionaires
-    ls = [list(job_data.values()) for job_data in data]
-
+    # create list of tuples of keys and values
+    lat_ls = [list(zip(job.keys(), job.values())) for job in data]
+        
     # order value with respect to reorder list
-    ordered_list = [[item[i] for i in reorder] for item in ls]
+    ordered_list = [[item[i] for i in reorder] for item in lat_ls]
+
+    # get keys
+    key_list = [ list(map(lambda x: x[0], k)) for k in ordered_list ][0]
+
+    # get values
+    value_list = [ list(map(lambda x: x[1], v)) for v in ordered_list ]
+
+    # unquantify values
+    final_data = [data_unquantification(item) for item in value_list]
 
     # unquantify datas
-    final_data = [data_unquantification(item) for item in ordered_list]
-
+    # final_data = [data_unquantification(item) for item in ordered_list]
+    # combine = [ list(zip(k, v)) for k,v in(key_list,value_list) ]
     # sort data on basis of similarity which is last item in list 
-    return {'data': sorted(final_data, key=itemgetter(-1), reverse=True)}
+    return { 'keys': key_list,
+        'values': sorted(final_data, key=itemgetter(-1), reverse=True)
+    }
